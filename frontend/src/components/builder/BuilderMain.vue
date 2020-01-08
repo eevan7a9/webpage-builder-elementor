36f557158ecdbbf3918ec3e19f03b290acef7055
eevan7a9 <e7a9@protonmail.com>
2020-01-09 05:54:34 +0900

    added builder main and fixed builder TopNav

---
+++ frontend/src/components/builder/BuilderMain.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="builder-main-wrapper container">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas voluptate
+      porro nobis repudiandae distinctio, voluptatem, accusamus voluptas dolor
+      officia, deserunt doloremque similique molestiae repellat! Architecto, hic
+      deleniti a ipsam nam doloribus culpa deserunt perspiciatis corporis sed
+      ipsum possimus quia iste excepturi molestias delectus facilis veniam
+      quisquam, dicta temporibus facere laboriosam provident odio fuga?
+      Accusantium modi ad alias ipsum beatae velit doloribus, molestias minus
+      laudantium ut, quis vitae harum. Fugiat magni amet repellat eos natus
+      ipsum quo impedit reprehenderit maiores, autem eaque quia a possimus
+      voluptas. Cumque nesciunt magni voluptas, laudantium totam beatae rerum
+      quod explicabo ab id esse. Quas quia at eveniet, possimus, est, nisi quo
+      dolores vel aliquid fuga dolorum! Neque harum explicabo consequatur!
+      Aperiam assumenda vero sequi labore eos soluta consequatur maxime quis
+      laborum necessitatibus. Ipsa recusandae iste, suscipit, dolore animi fuga
+      iusto perspiciatis, aut ex fugiat repellendus magni nesciunt laborum
+      eveniet nemo reiciendis optio sit totam dicta doloribus similique! Sequi,
+      hic. Facere optio accusantium deserunt ducimus similique minima veritatis,
+      nemo debitis, quis explicabo eos perspiciatis in deleniti officiis
+      laudantium! Aliquam suscipit officiis quae provident, quam laboriosam
+      quaerat impedit! Omnis dolores sunt dolor voluptatibus quaerat, quam at
+      libero sequi similique facere ipsum mollitia? At aperiam nihil quo saepe.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas voluptate
+      porro nobis repudiandae distinctio, voluptatem, accusamus voluptas dolor
+      officia, deserunt doloremque similique molestiae repellat! Architecto, hic
+      deleniti a ipsam nam doloribus culpa deserunt perspiciatis corporis sed
+      ipsum possimus quia iste excepturi molestias delectus facilis veniam
+      quisquam, dicta temporibus facere laboriosam provident odio fuga?
+      Accusantium modi ad alias ipsum beatae velit doloribus, molestias minus
+      laudantium ut, quis vitae harum. Fugiat magni amet repellat eos natus
+      ipsum quo impedit reprehenderit maiores, autem eaque quia a possimus
+      voluptas. Cumque nesciunt magni voluptas, laudantium totam beatae rerum
+      quod explicabo ab id esse. Quas quia at eveniet, possimus, est, nisi quo
+      dolores vel aliquid fuga dolorum! Neque harum explicabo consequatur!
+      Aperiam assumenda vero sequi labore eos soluta consequatur maxime quis
+      laborum necessitatibus. Ipsa recusandae iste, suscipit, dolore animi fuga
+      iusto perspiciatis, aut ex fugiat repellendus magni nesciunt laborum
+      eveniet nemo reiciendis optio sit totam dicta doloribus similique! Sequi,
+      hic. Facere optio accusantium deserunt ducimus similique minima veritatis,
+      nemo debitis, quis explicabo eos perspiciatis in deleniti officiis
+      laudantium! Aliquam suscipit officiis quae provident, quam laboriosam
+      quaerat impedit! Omnis dolores sunt dolor voluptatibus quaerat, quam at
+      libero sequi similique facere ipsum mollitia? At aperiam nihil quo saepe.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas voluptate
+      porro nobis repudiandae distinctio, voluptatem, accusamus voluptas dolor
+      officia, deserunt doloremque similique molestiae repellat! Architecto, hic
+      deleniti a ipsam nam doloribus culpa deserunt perspiciatis corporis sed
+      ipsum possimus quia iste excepturi molestias delectus facilis veniam
+      quisquam, dicta temporibus facere laboriosam provident odio fuga?
+      Accusantium modi ad alias ipsum beatae velit doloribus, molestias minus
+      laudantium ut, quis vitae harum. Fugiat magni amet repellat eos natus
+      ipsum quo impedit reprehenderit maiores, autem eaque quia a possimus
+      voluptas. Cumque nesciunt magni voluptas, laudantium totam beatae rerum
+      quod explicabo ab id esse. Quas quia at eveniet, possimus, est, nisi quo
+      dolores vel aliquid fuga dolorum! Neque harum explicabo consequatur!
+      Aperiam assumenda vero sequi labore eos soluta consequatur maxime quis
+      laborum necessitatibus. Ipsa recusandae iste, suscipit, dolore animi fuga
+      iusto perspiciatis, aut ex fugiat repellendus magni nesciunt laborum
+      eveniet nemo reiciendis optio sit totam dicta doloribus similique! Sequi,
+      hic. Facere optio accusantium deserunt ducimus similique minima veritatis,
+      nemo debitis, quis explicabo eos perspiciatis in deleniti officiis
+      laudantium! Aliquam suscipit officiis quae provident, quam laboriosam
+      quaerat impedit! Omnis dolores sunt dolor voluptatibus quaerat, quam at
+      libero sequi similique facere ipsum mollitia? At aperiam nihil quo saepe.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas voluptate
+      porro nobis repudiandae distinctio, voluptatem, accusamus voluptas dolor
+      officia, deserunt doloremque similique molestiae repellat! Architecto, hic
+      deleniti a ipsam nam doloribus culpa deserunt perspiciatis corporis sed
+      ipsum possimus quia iste excepturi molestias delectus facilis veniam
+      quisquam, dicta temporibus facere laboriosam provident odio fuga?
+      Accusantium modi ad alias ipsum beatae velit doloribus, molestias minus
+      laudantium ut, quis vitae harum. Fugiat magni amet repellat eos natus
+      ipsum quo impedit reprehenderit maiores, autem eaque quia a possimus
+      voluptas. Cumque nesciunt magni voluptas, laudantium totam beatae rerum
+      quod explicabo ab id esse. Quas quia at eveniet, possimus, est, nisi quo
+      dolores vel aliquid fuga dolorum! Neque harum explicabo consequatur!
+      Aperiam assumenda vero sequi labore eos soluta consequatur maxime quis
+      laborum necessitatibus. Ipsa recusandae iste, suscipit, dolore animi fuga
+      iusto perspiciatis, aut ex fugiat repellendus magni nesciunt laborum
+      eveniet nemo reiciendis optio sit totam dicta doloribus similique! Sequi,
+      hic. Facere optio accusantium deserunt ducimus similique minima veritatis,
+      nemo debitis, quis explicabo eos perspiciatis in deleniti officiis
+      laudantium! Aliquam suscipit officiis quae provident, quam laboriosam
+      quaerat impedit! Omnis dolores sunt dolor voluptatibus quaerat, quam at
+      libero sequi similique facere ipsum mollitia? At aperiam nihil quo saepe.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.builder-main-wrapper {
+  padding-top: 70px;
+}
+</style>
--- frontend/src/components/builder/BuilderTopbar.vue
+++ frontend/src/components/builder/BuilderTopbar.vue
@@ -1,28 +1,26 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand @click="toggleBuilderSidebar" v-if="!isSidebarOpen">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <polyline points="13 17 18 12 13 7" />
-          <polyline points="6 17 11 12 6 7" />
-        </svg>
-      </b-navbar-brand>
-      <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand @click="toggleBuilderSidebar" v-if="!isSidebarOpen">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <polyline points="13 17 18 12 13 7" />
+        <polyline points="6 17 11 12 6 7" />
+      </svg>
+    </b-navbar-brand>
+    <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="ml-auto">right</b-navbar-nav>
         </b-collapse> -->
-    </b-navbar>
-  </div>
+  </b-navbar>
 </template>
 
 <script>
@@ -39,7 +37,9 @@ export default {
 
 <style lang="scss" scoped>
 .navbar {
+  position: fixed;
   height: 56px;
+  width: 100%;
   .navbar-brand {
     cursor: pointer;
   }
