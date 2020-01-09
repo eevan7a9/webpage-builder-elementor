59267c0886d4cb35dc868b2bbefbe4310e91f015
eevan7a9 <e7a9@protonmail.com>
2020-01-09 19:24:15 +0900

    added section Items

---
+++ frontend/src/components/section/SectionItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="section-item">
+    <div class="section-options d-flex justify-content-end mr-1">
+      <button class="btn text-light p-1 mr-1 border">
+        <img src="@/assets/icons/add-icon.svg" />
+      </button>
+      <button class="btn text-light  p-1 mr-1 border">
+        <img src="@/assets/icons/trash-icon.svg" />
+      </button>
+    </div>
+    <div class="rounded item-content">
+      <h1>Sections</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.section-item {
+  .section-options {
+    visibility: hidden;
+    button {
+      height: 0;
+      width: 0;
+      transition: 0.5s;
+      background: #b5b5b5;
+      img {
+        height: 0;
+        width: 0;
+        transition: 0.5s;
+      }
+    }
+  }
+  .item-content {
+    border: 3px solid #b5b5b5;
+  }
+  &:hover {
+    .section-options {
+      visibility: visible;
+      button {
+        height: 35px;
+        width: 35px;
+        transition: 0.5s;
+        background: #17a2b8;
+        img {
+          height: 18px;
+          width: 18px;
+          transition: 0.5s;
+        }
+      }
+    }
+    .item-content {
+      border-color: #17a2b8;
+    }
+  }
+}
+</style>
--- frontend/src/components/section/SectionList.vue
+++ frontend/src/components/section/SectionList.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="section-wrapper">
+    <SectionItem />
+    <hr />
     <SectionAdd />
   </div>
 </template>
 
 <script>
+import SectionItem from "@/components/section/SectionItem.vue";
 import SectionAdd from "@/components/section/SectionAdd.vue";
 export default {
   components: {
+    SectionItem,
     SectionAdd
   }
 };
