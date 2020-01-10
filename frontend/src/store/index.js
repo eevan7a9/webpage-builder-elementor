f5fad776276f97557b4b6eb8c14eda3d458a4ab8
eevan7a9 <e7a9@protonmail.com>
2020-01-10 18:55:04 +0900

    added custom style

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import columns from "./modules/columns.js";
 import builderSidebar from "./modules/builder-sidebar.js";
-import sections from "./modules/sections.js";
+import page from "./modules/page.js";
 
 Vue.use(Vuex);
 
@@ -10,6 +10,6 @@ export default new Vuex.Store({
   modules: {
     columns,
     builderSidebar,
-    sections
+    page
   }
 });
