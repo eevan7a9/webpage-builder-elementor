94dd796f8ae66c998dbd08fc4e7dca3824ff3056
eevan7a9 <e7a9@protonmail.com>
2020-01-13 03:54:31 +0900

    enable adding elements on empty column

--- frontend/src/components/element/ElementList.vue
+++ frontend/src/components/element/ElementList.vue
@@ -1,9 +1,31 @@
 <template>
-  <div class="element-wrapper ">
-    <draggable v-model="myElements" v-bind="dragOptions">
+  <div class="element-wrapper h-100">
+    <draggable v-model="myElements" v-bind="dragOptions" class="h-100">
       <transition-group type="transition" name="flip-list">
         <div v-for="element in myElements" :key="element.id" class="w-100 p-1">
           {{ element }} ,{{ columnId }} {{ rowId }} {{ sectionId }}
+        </div>
+        <div
+          class="empty w-100 p-1 h-100 d-flex justify-content-center align-items-center"
+          :key="1"
+          v-if="myElements.length < 1"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="m-4"
+          >
+            <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+            <line x1="12" y1="8" x2="12" y2="16" />
+            <line x1="8" y1="12" x2="16" y2="12" />
+          </svg>
         </div>
       </transition-group>
     </draggable>
@@ -49,4 +71,18 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.element-wrapper {
+  .empty {
+    cursor: pointer;
+    svg {
+      color: #b5b5b5;
+    }
+  }
+  &:hover {
+    svg {
+      color: #17a2b8;
+    }
+  }
+}
+</style>
